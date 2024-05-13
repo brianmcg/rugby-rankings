a0b9a4ead2ad15c5bc77840f8999a6db8747c4f4
Brian McGrath <brian.joseph.mcgrath@gmail.com>
2024-05-13 22:56:13 +0100

    Removed unnecessary info from match list item

--- src/App/components/Main/components/Matches/components/Match.jsx
+++ src/App/components/Main/components/Matches/components/Match.jsx
@@ -5,35 +5,38 @@ import Paper from '@mui/material/Paper';
 import Typography from '@mui/material/Typography';
 import DeleteIcon from '@mui/icons-material/Delete';
 import EditIcon from '@mui/icons-material/Edit';
-import Breadcrumb from '@components/Breadcrumb';
+import Translate from '@components/Translate';
 import { SUCCESS, ERROR } from '@constants/colors';
-import { formatDay } from '@utils/date'; 
-import { getColor, getMatchResult } from './helpers';
+import { getColor } from './helpers';
 
-function renderMatchInfo(match) {
-  const { time, venue,competition } = match;
-  const date = time ? formatDay(time?.millis) : null;
-  const country = venue?.country ? `@ ${venue.country}` : null;
+// function renderMatchInfo(match) {
+//   const { time, venue,competition } = match;
+//   const date = time ? formatDay(time?.millis) : null;
+//   const country = venue?.country ? `@ ${venue.country}` : null;
   
-  return (
-    <Stack direction="row" spacing={1} mb={1} justifyContent="flex-start" sx={{ opacity: 0.6 }}>
-      <Breadcrumb>
-        { competition ? <Typography variant="caption">{competition}</Typography> : null }
-        { date ? <Typography variant="caption">{date}</Typography> : null }
-        { country ? <Typography variant="caption">{country}</Typography> : null }
-      </Breadcrumb>
-    </Stack>
-  );
-}
+//   return (
+//     <Stack direction="row" spacing={1} mb={1} justifyContent="flex-start" sx={{ opacity: 0.6 }}>
+//       <Breadcrumb>
+//         { competition ? <Typography variant="caption">{competition}</Typography> : null }
+//         { date ? <Typography variant="caption">{date}</Typography> : null }
+//         { country ? <Typography variant="caption">{country}</Typography> : null }
+//       </Breadcrumb>
+//     </Stack>
+//   );
+// }
 
 function renderMatchResult({ match, color }) {
-  const { homeTeam, awayTeam } = match;
+  const { homeTeam, awayTeam, homeScore, awayScore, isComplete } = match;
   return (
     <Stack>
       <Grid container direction="row" gap={1} alignItems="center" justifyContent="flex-start">
         <Typography variant="body1">{homeTeam.name}</Typography>
         <Typography variant="h6" color={color}>
-          {getMatchResult(match)}
+          {
+            isComplete
+              ? `${homeScore} - ${awayScore}`
+              : <Translate text="app.main.matches.vs"></Translate>
+          }
         </Typography>
         <Typography variant="body1">{awayTeam.name}</Typography>
       </Grid>
@@ -70,8 +73,6 @@ export default function Match({ match, onSelectMatch, onRemoveMatch }) {
   
   return (
     <Paper elevation={3} sx={{ padding: 2, width: '100%', borderLeft: `solid 5px ${color}` }} >
-      {renderMatchInfo(match)}
-
       <Grid container direction="row" alignItems="center" justifyContent="space-between">
         {renderMatchResult({ match, color })}
         {renderButtons(options)}
