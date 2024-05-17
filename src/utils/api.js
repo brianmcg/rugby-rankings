11267d087f93024a78acc893d020865e83cd9c61
Brian McGrath <brian.joseph.mcgrath@gmail.com>
2024-05-17 19:53:12 +0100

    Refactored api and parser

--- src/utils/api.js
+++ src/utils/api.js
@@ -6,7 +6,8 @@ import { RANKINGS, FIXTURES, TEAMS } from '@constants/urls';
 import { VALUES } from '@constants/sports';
 
 // This is for slowing down requests for development purposes.
-// const sleep = t => new Promise(resolve => setTimeout(resolve, t));
+// eslint-disable-next-line
+const sleep = t => new Promise(resolve => setTimeout(resolve, t));
 
 // This is for fetching old rankings for development purposes.
 // const currentDate = subtractWeeks(new Date(), 2);
@@ -25,14 +26,23 @@ async function axiosGet(url, params = {}) {
   }
 }
 
+export async function fetchCountries(teamIds) {
+  const requests = teamIds.map(id => axiosGet(`${TEAMS}/${id}`));
+  const teams = await axios.all(requests);
+
+  return teams.reduce((memo, team) => ({ ...memo, [team.id]: team.country }), {});
+}
+
 function fetchRankings(sport = VALUES.MENS) {
   return axiosGet(`${RANKINGS}/${sport}`, {
     date: formatApiDate(currentDate),
   });
 }
 
-function fetchMatches(sport = VALUES.MENS, startDate, endDate) {
-  return axiosGet(FIXTURES, {
+async function fetchMatches(sport = VALUES.MENS, startDate, endDate, rankings) {
+  const rankedTeamIds = rankings.map(entry => entry.team.id);
+
+  const response = await axiosGet(FIXTURES, {
     startDate: formatApiDate(startDate),
     endDate: formatApiDate(endDate),
     sort: 'asc',
@@ -40,67 +50,57 @@ function fetchMatches(sport = VALUES.MENS, startDate, endDate) {
     page: 0,
     sport,
   });
-}
 
-export function fetchTeams(ids) {
-  const requests = ids.map(id => axiosGet(`${TEAMS}/${id}`));
+  // filter out matches with undefined or null teams or that feature teams that are not in the rankings.
+  const matches = response.content.filter(match => {
+    const isTeamsConfirmed = match.teams.every(team => Boolean(team));
+    const isTeamsRanked = match.teams.every(team => rankedTeamIds.includes(team.id));
 
-  return axios.all(requests);
+    return isTeamsConfirmed && isTeamsRanked;
+  });
+
+  // For each match I need to fetch the country for each participating team.
+  // The name of thecountry can be different from the name of team.
+  // I need the name of the country later to compare with the venue country to to determine home advantage.
+  return Promise.all(matches.map(async match => {
+    const matchTeamIds = match.teams.map(team => team.id);
+    const countries = await fetchCountries(matchTeamIds);
+
+    return {
+      ...match,
+      teams: match.teams.map(team => ({ ...team, country: countries[team.id] })),
+    };
+  }));
 }
 
 export async function fetchData(sport) {
   try {
-    // Fetch the rankings first.
     const { entries: rankings, label, effective } = await fetchRankings(sport);
 
-
-
-    // Get thestart and end date for which to fetch matches.
     const startDate = effective.millis;
-    const endDate = addWeeks(startDate, dateRange).valueOf();
+    const endDate = addWeeks(startDate, dateRange);
 
-    // Fetch the matches.
-    const matches = await fetchMatches(sport, startDate, endDate);
+    const matches = await fetchMatches(sport, startDate, endDate, rankings);
 
-    // Get id's of teams participating in matches in order to fetch
-    // the country data required to determine home advantage.
-    const ids = matches.content.reduce(
-      (matchIds, match) => match.teams.reduce(
-        (teamIds, { id }) => teamIds.includes(id) ? teamIds : [...teamIds, id],
-        matchIds,
+    // Make a map of teams participating in matches grouped by team id to be used below.
+    const matchTeamsById = matches.reduce(
+      (outerMemo, match) => (match.teams.reduce(
+        (innerMemo, team) => ({ ...innerMemo, [team.id]: team }),
+        outerMemo)
       ),
-      [],
+      {},
     );
 
-    // I need to fetch each team that has a match, to get the name of the country,
-    // which can be different from the name of team. I need the name of the country
-    // later to compare with the venue country to to determine home advantage.
-    const matchParticipants = await fetchTeams(ids);
-
-    // Create a map with team ids as the key and country as the value.
-    const countriesByTeamId = matchParticipants.reduce((memo, team) => ({
-      ...memo,
-      [team.id]: team.country,
-    }), {});
-
-    // Create a list of teams from countries in the rankings.
-    // This will be used for the autocomplete team input, and also to exclude
-    // matches that have teams that are not in the rankings.
-    const teams = rankings.map(entry => entry.team);
-
-    // Create a list of teams with name of the country injected
-    // for teams that have matches in the range.
-    const teamsWithCountry = teams.map(team => ({
-      ...team,
-      country: countriesByTeamId[team.id],
-    }));
+    // Create a list of teams from the fetched rankings, with the the teams participating in matches injected,
+    // since they have the country attribute missing from the teams fetched from the rankings.
+    const teams = rankings.map(({ team }) => matchTeamsById[team.id] ?? team, []);
 
     return {
       sport,
       label,
-      teams: teamsWithCountry,
+      teams,
       rankings,
-      matches: parseMatches(matches, teamsWithCountry),
+      matches: parseMatches(matches),
       startDate,
       endDate,
     };
--- src/utils/parsers.js
+++ src/utils/parsers.js
@@ -1,75 +1,40 @@
-const hasNoHomeAdvantage = [
-  /Asia Rugby Championship \w+/gm,
+// I need to check if home advantage applies in a competition.
+// For example I've seen that home advantage didn't seem apply to the ranking calculations for the `Asia Rugby Championship 2024`.
+// I see nothing in the api data that can tell me this.
+// So I'm just going to keep a list `competitionsWithNoHomeAdvantage` here in the code.
+const competitionsWithNoHomeAdvantage = [
+  /Asia Rugby Championship \w+/i,
 ];
 
-function parseMatch(match ,teams) {
-  const {
-    venue,
-    teams: matchTeams = [],
-    scores = [],
-    status,
-    matchId,
-    time,
-    competition,
-  } = match;
-
-  // Check if home advantage applies in this competition.
-  // I see nothing in the api data that can tell me this.
-  // So I'm just going to keep a list `hasNoHomeAdvantage` here in the code and update
-  // it whenever I see results where home advantage didn't apply. For example I've seen that
-  // home advantage didn't seem apply to the ranking calculations for the `Asia Rugby Championship 2024`.
-  const noHomeAdvantage = hasNoHomeAdvantage.some(regex => Boolean(competition?.match(regex)));
-
-  // I want to use the teams from the `teams` array rather than the match data since
-  // they contain the country name of the team, which can be different from the
-  // `name` of the team. Eg. for Moldova the team name is `Moldova` but the country
-  // name is `Moldova, Republic of`. I compare this country name with the venue country below to
-  // determine which team has home advantage.
-  const participants = matchTeams.map(m => teams.find(t => t.id === m?.id) || null);
-
-  // Determine the index of the team with home advantage.
-  const indexOfVenueTeam = participants.map(t => t.country).indexOf(venue?.country);
+function parseMatch({ matchId, teams, scores, status, venue, time, competition }) {
+  const noHomeAdvantage = competitionsWithNoHomeAdvantage.some(regex => Boolean(competition?.match(regex)));
+  const indexOfVenueTeam = teams.map(t => t.country).indexOf(venue?.country);
   const isNeutralVenue = noHomeAdvantage ? true : indexOfVenueTeam < 0;
   const homeIndex = indexOfVenueTeam < 0 ? 0 : indexOfVenueTeam;
   const awayIndex = indexOfVenueTeam === 1 ? 0 : 1;
-
-  const homeTeam = teams.find(team => team.id === participants[homeIndex]?.id) || null;
-  const awayTeam = teams.find(team => team.id === participants[awayIndex]?.id) || null;
-
+  const homeTeam = teams[homeIndex];
+  const awayTeam = teams[awayIndex];
   const isComplete = status === 'C';
   const homeScore = isComplete ? scores[homeIndex] : null;
   const awayScore = isComplete ? scores[awayIndex] : null;
-  const isWorldCup = /Rugby World Cup \w+/gm.test(competition);
+  const isWorldCup = (/rugby world cup \w+/i).test(competition);
 
   return {
+    matchId,
     homeTeam,
     awayTeam,
     homeScore,
     awayScore,
+    venue,
+    time,
+    competition,
     isNeutralVenue,
     isWorldCup,
     isComplete,
-    venue,
-    matchId,
-    time,
-    competition,
     isCreated: false,
   };
 }
 
-export function parseMatches(matches, teams) {
-  const teamIds = teams.map(team => team.id);
-
-  return matches.content.reduce((memo, match) => {
-    // Are both teams in the match present in the list of ranked teams.
-    const isTeamsRanked = match.teams.reduce((memo, team) => memo && teamIds.includes(team.id), true);
-    // Are both teams in teh match defined.
-    const isTeamsConfirmed = match.teams.reduce((memo, team) => memo && Boolean(team), true);
-
-    if (isTeamsRanked && isTeamsConfirmed) {
-      return [...memo, parseMatch(match, teams)];
-    }
-
-    return memo;
-  }, []);
+export function parseMatches(matches) {
+  return matches.map(match => parseMatch(match));
 }
