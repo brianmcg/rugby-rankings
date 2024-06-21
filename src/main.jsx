59affcb9593fa8237c475a6b4e23c4deaba457c5
Brian McGrath <brian.joseph.mcgrath@gmail.com>
2024-06-21 18:05:18 +0100

    Refactored code

--- src/main.jsx
+++ src/main.jsx
@@ -1,28 +1,11 @@
-// import { StrictMode } from 'react';
+import { StrictMode } from 'react';
 import ReactDOM from 'react-dom/client';
-import { I18nextProvider } from 'react-i18next';
-// import { ParallaxProvider } from 'react-scroll-parallax';
-import { CssBaseline } from '@mui/material/';
-import { ThemeProvider  } from '@mui/material/styles';
-import i18n from '@utils/i18n';
 import App from './App';
-import theme from './theme';
 
-// import '@fontsource/roboto/300.css';
-// import '@fontsource/roboto/400.css';
-// import '@fontsource/roboto/500.css';
-// import '@fontsource/roboto/700.css';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  // <StrictMode>
-  // <ParallaxProvider>
-  <I18nextProvider i18n={i18n}>
-    <ThemeProvider theme={theme}>
-      <CssBaseline/>
-      <App />
-    </ThemeProvider>
-  </I18nextProvider>,
-  // </ParallaxProvider>
-  // </StrictMode>,
+  <StrictMode>
+    <App />
+  </StrictMode>,
 );
